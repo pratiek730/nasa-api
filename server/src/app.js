71a215eee5835a2f06f7e5b4e2ceeac23b6fe13b
Pratiek Parashar <prateek.parashar730@gmail.com>
2022-06-26 18:06:12 +0530

    Added version of API and updated the client for the same

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const planetsRouter = require('./routes/planets/planets.router');
+
 const cors = require('cors');
 const app = express();
 const path = require('path');
-const { launchesRouter } = require('./routes/launches/launches.router');
+const api = require('./routes/api');
+
 
 app.use(cors());
 app.use(express.json());
-app.use('/planets',planetsRouter);
-app.use('/launches',launchesRouter);
+
 app.use(express.static(path.join(__dirname,'..','public')));
+
+app.use("/v1",api);
 
 app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname,'..','public','index.html'));
