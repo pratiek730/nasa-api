b18dcc9c0c047ce2f93967aa7d5dd56a3d47a79b
Pratiek Parashar <prateek.parashar730@gmail.com>
2022-07-01 13:02:11 +0530

    Added dotenv and secured mongo link and password by using .env file and loading it's data using dotenv.config()

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,21 @@
 const http = require('http');
-const mongoose = require('mongoose');
 const { loadPlanetsData } = require('./models/planets.module');
+require('dotenv').config();
 
 
 PORT = process.env.PORT || 8000 ;
-const MONGO_URL = 'mongodb://nasa-api:sf8FctdeVqPf3dHn@ac-35d0idq-shard-00-00.gwqkoxh.mongodb.net:27017,ac-35d0idq-shard-00-01.gwqkoxh.mongodb.net:27017,ac-35d0idq-shard-00-02.gwqkoxh.mongodb.net:27017/?ssl=true&replicaSet=atlas-8k6va9-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 
 const app = require('./app');
 const { loadLaunchData } = require('./models/launches.model');
+const { mongoConnect } = require('./services/mongo');
 const server = http.createServer(app);
 
 
-mongoose.connection.once('open', () => {
-    console.log('Mongoose connection is ready'); 
-});
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-});
+
 
 async function startServer(){
-    await mongoose.connect(MONGO_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoConnect();
     await loadPlanetsData();
     await loadLaunchData();
     server.listen(PORT,"0.0.0.0", () => {
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb://nasa-api:sf8FctdeVqPf3dHn@ac-35d0idq-shard-00-00.gwqkoxh.mongodb.net:27017,ac-35d0idq-shard-00-01.gwqkoxh.mongodb.net:27017,ac-35d0idq-shard-00-02.gwqkoxh.mongodb.net:27017/?ssl=true&replicaSet=atlas-8k6va9-shard-0&authSource=admin&retryWrites=true&w=majority'
+const MONGO_URL = process.env.MONGO_URL;
 
 
 mongoose.connection.once('open', () => {
