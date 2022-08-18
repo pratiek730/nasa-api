6815a58cb6e7e8fb913e4493fa81eb3811fb8c3f
Pratiek Parashar <prateek.parashar730@gmail.com>
2022-08-18 16:44:01 +0530

    testing the workflow 2

--- server/src/app.js
+++ server/src/app.js
@@ -13,8 +13,6 @@ app.use(express.static(path.join(__dirname,'..','public')));
 
 app.use("/v1",api);
 
-
-
 app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname,'..','public','index.html'));
 })
